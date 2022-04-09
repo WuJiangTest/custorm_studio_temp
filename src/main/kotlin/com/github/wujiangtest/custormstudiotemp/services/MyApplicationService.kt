743295971dbf800497ed2afc0a2b654e7abc574f
GitHub Action <action@github.com>
2022-04-09 04:00:37 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/wujiangtest/custormstudiotemp/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.wujiangtest.custormstudiotemp.services
+
+import com.github.wujiangtest.custormstudiotemp.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
